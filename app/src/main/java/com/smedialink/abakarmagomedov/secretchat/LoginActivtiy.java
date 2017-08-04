b29372f8efd3fc4c1135ff730d8036c0e818add4
abakarmagomedov <skat131998@mail.ru>
2017-08-04 19:48:53 +0300

    Initial commit

---
+++ app/src/main/java/com/smedialink/abakarmagomedov/secretchat/LoginActivtiy.java
@@ -0,0 +1,13 @@
+package com.smedialink.abakarmagomedov.secretchat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivtiy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activtiy);
+    }
+}
