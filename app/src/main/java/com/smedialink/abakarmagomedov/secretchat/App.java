c5990d06b1e419d5f5776eda3cbb94effd1143a9
abakarmagomedov <skat131998@mail.ru>
2017-08-11 19:12:02 +0300

    work with qiscus chat

--- app/src/main/java/com/smedialink/abakarmagomedov/secretchat/App.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/secretchat/App.java
@@ -13,6 +13,6 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        Qiscus.init(this, "78aa547bd344f139e0b2edf98a3f3d60");
+        Qiscus.init(this, "durovchat");
     }
 }
--- app/src/main/java/com/smedialink/abakarmagomedov/secretchat/presentation/login/LoginActivtiy.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/secretchat/presentation/login/LoginActivtiy.java
@@ -1,15 +1,25 @@
 package com.smedialink.abakarmagomedov.secretchat.presentation.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.qiscus.sdk.Qiscus;
+import com.qiscus.sdk.data.model.QiscusAccount;
+import com.qiscus.sdk.data.model.QiscusChatRoom;
+import com.qiscus.sdk.ui.QiscusGroupChatActivity;
 import com.smedialink.abakarmagomedov.secretchat.BaseActivity;
 import com.smedialink.abakarmagomedov.secretchat.R;
+
+import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import retrofit2.HttpException;
 
 public class LoginActivtiy extends BaseActivity implements LoginView {
 
@@ -17,6 +27,61 @@ public class LoginActivtiy extends BaseActivity implements LoginView {
 
     @OnClick(R.id.log_in_button)
     void onButtonClick(){
+
+        Qiscus.setUser("user@email.com", "userKey")
+                .withUsername("Tony Stark")
+                .withAvatarUrl("http://avatar.url.com/handsome.jpg")
+                .save(new Qiscus.SetUserListener() {
+                    @Override
+                    public void onSuccess(QiscusAccount qiscusAccount) {
+                        Qiscus.buildGroupChatRoomWith("UniqueId")
+                                .withName("RoomName")
+                                .withAvatar("http://avatar.url.com/group.jpg")
+                                .build(new Qiscus.ChatBuilderListener() {
+                                    @Override
+                                    public void onSuccess(QiscusChatRoom qiscusChatRoom) {
+                                        startActivity(QiscusGroupChatActivity.generateIntent(LoginActivtiy.this, qiscusChatRoom));
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable throwable) {
+                                        if (throwable instanceof HttpException) { //Error response from server
+                                            HttpException e = (HttpException) throwable;
+                                            try {
+                                                String errorMessage = e.response().errorBody().string();
+                                                Log.e("TAG", errorMessage);
+                                                showError(errorMessage);
+                                            } catch (IOException e1) {
+                                                e1.printStackTrace();
+                                            }
+                                        } else if (throwable instanceof IOException) { //Error from network
+                                            showError("Can not connect to qiscus server!");
+                                        } else { //Unknown error
+                                            showError("Unexpected error!");
+                                        }
+                                    }
+                                });
+                    }
+                    @Override
+                    public void onError(Throwable throwable) {
+                        if (throwable instanceof HttpException) { //Error response from server
+                            HttpException e = (HttpException) throwable;
+                            try {
+                                String errorMessage = e.response().errorBody().string();
+                                Log.e("TAG", errorMessage);
+                                showError(errorMessage);
+                            } catch (IOException e1) {
+                                e1.printStackTrace();
+                            }
+                        } else if (throwable instanceof IOException) { //Error from network
+                            showError("Can not connect to qiscus server!");
+                        } else { //Unknown error
+                            showError("Unexpected error!");
+                        }
+                    }
+                });
+
+
     }
 
     @Override
@@ -28,7 +93,7 @@ public class LoginActivtiy extends BaseActivity implements LoginView {
 
     @Override
     public void showError(String message) {
-
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 
     @Override
