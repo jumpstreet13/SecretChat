eb7ed7a91199ceb0caf054583ec753829139dd47
abakarmagomedov <skat131998@mail.ru>
2017-12-14 17:18:47 +0300

    divider item decoration is added

--- app/src/main/java/com/smedialink/abakarmagomedov/secretchat/presentation/user_list/UserListActivity.java
+++ app/src/main/java/com/smedialink/abakarmagomedov/secretchat/presentation/user_list/UserListActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 
 import com.smedialink.abakarmagomedov.secretchat.R;
 import com.smedialink.abakarmagomedov.secretchat.domain.entity.UserEntity;
+import com.smedialink.abakarmagomedov.secretchat.presentation.user_list.adapter.DividerItemDecoration;
 import com.smedialink.abakarmagomedov.secretchat.presentation.user_list.adapter.UserListAdapter;
 
 import java.util.ArrayList;
@@ -28,10 +29,20 @@ public class UserListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_list);
         ButterKnife.bind(this);
+        RecyclerView.ItemDecoration dividerItemDecoration = new DividerItemDecoration(getResources().getDrawable(R.drawable.divider_blue));
         adapter = new UserListAdapter(R.layout.user_item, Arrays.asList(new UserEntity("user1", "user1"),
                 new UserEntity("user2", "user2"),
-                new UserEntity("user3", "user3")));
+                new UserEntity("user3", "user3"),
+                new UserEntity("user4", "user2"),
+                new UserEntity("user5", "user2"),
+                new UserEntity("user6", "user2"),
+                new UserEntity("user7", "user2"),
+                new UserEntity("user8", "user2"),
+                new UserEntity("user9", "user2"),
+                new UserEntity("user10", "user2"),
+                new UserEntity("user11", "user2")));
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.addItemDecoration(dividerItemDecoration);
         recyclerView.setAdapter(adapter);
     }
 
